56b64e0ae3a8d17867077cfbb573d2f0f84516f1
pvczku <99214998+pvczku@users.noreply.github.com>
2022-03-11 15:27:45 +0100

    changes
    need to fix js asap

--- script.js
+++ script.js
@@ -2,12 +2,54 @@ var bill = document.getElementById("bill");
 var wynik = document.getElementById("wynik");
 var people = document.getElementById("people");
 let f;
+let percentValue;
+let tip;
 
-function calc(n) {
-    a = bill.value;
-    b = people.value;
-    wynik.innerText = n*a/b;
-    wynik.style.color = "red";
+
+function percent(e) {
+    percentValue = e;
+    
+    calcTip();
+    calcTotal();
+    infNaN();
+}
+
+function tipNTotal() {
+    
+    a = document.getElementById("bill").value;
+    b = document.getElementById("people").value;
+    
+    calcTip();
+    calcTotal();
+    infNaN();
+}
+
+
+// POPRACOWAC NAD TYM ABY NIE POJAWIALO SIE INFINITY ANI NaN
+
+
+// function infNaN() {    
+//     if ((document.getElementById("tipamount").innerText = Infinity + "$") || (document.getElementById("total").innerText = Infinity + "$")) {
+//         document.getElementById("tipamount") = "0.00$";
+//         document.getElementById("total") = "0.00$";
+//     }
+//     else if ((document.getElementById("tipamount").innerText = "Infinity$") || (document.getElementById("total").innerText = "NaN$")) {
+//         document.getElementById("tipamount") = "0.00$";
+//         document.getElementById("total") = "0.00$";
+//     }
+// }
+
+function calcTip() {
+    a = document.getElementById("bill").value;
+    b = document.getElementById("people").value;
+    console.log((a*percentValue/b));
+    tip = (a*percentValue/b);
+    document.getElementById("tipamount").innerText = (Math.round(tip * 100)/100 + "$");
+    console.log(Math.round(tip * 100)/100);
+}
+
+function calcTotal() {
+    document.getElementById("total").innerText = ((Math.round(tip * 100)/100) + a/b + "$");
 }
 
 function showNumber() {
@@ -20,4 +62,11 @@ function showNumber() {
         f = true;
     }
     console.log(f);
+}
+
+function reset() {
+    document.getElementById("bill").value = 0;
+    document.getElementById("people").value = 0;
+    document.getElementById("tipamount").innerText = "0.00$";
+    document.getElementById("total").innerText = "0.00$";
 }
